19a81a5a9ef49ec4ab51265499f9b88579f5ab99
Chris Kibble <chris@ckib.me>
2021-04-19 22:39:49 -0400

    Finalized CSV Pester Tests

--- Pester/csv.tests.ps1
+++ Pester/csv.tests.ps1
@@ -7,42 +7,48 @@ Describe "Ensure CSV Log Files Import Successfully" {
     It "Validate CSV Import Works as Expected Importing File"  {          
         $log = Import-LogSloth -LogFile $csvFile -SkipWarning
         $log.logData.count | Should -BeExactly 128    
+        $log.logType | Should -Be "CSV"
     }
 
     It "Validate CSV Import Works as Expected Importing Data" {
         $logData = Get-Content $csvFile -Raw
         $log = Import-LogSLoth -LogData $logData -SkipWarning
         $log.logData.count | Should -BeExactly 128    
+        $log.logType | Should -Be "CSV"
     }
 
     It "Validate CSV Import Works as Expected Importing Data from Pipeline" {
         $logData = Get-Content $csvFile -Raw
         $log = $logData | Import-LogSloth -SkipWarning
         $log.logData.count | Should -BeExactly 128    
+        $log.logType | Should -Be "CSV"
     }
 
     It "Validate CSV Sanitization Import Works as Expected Importing File"  {          
         $log = Import-LogSlothSanitized -LogFile $csvFile -SkipWarning
         $log.logData.count | Should -BeExactly 128    
+        $log.logType | Should -Be "CSV"
     }
 
     It "Validate CSV Sanitized Import Works as Expected Importing Data" {
         $logData = Get-Content $csvFile -Raw
         $log = Import-LogSLothSanitized -LogData $logData -SkipWarning
         $log.logData.count | Should -BeExactly 128    
+        $log.logType | Should -Be "CSV"
     }
 
     It "Validate CSV Sanitized Import Works as Expected Importing Data from Pipeline" {
         $logData = Get-Content $csvFile -Raw
         $log = $logData | Import-LogSlothSanitized -SkipWarning
         $log.logData.count | Should -BeExactly 128    
+        $log.logType | Should -Be "CSV"
     }
 
     It "Validate CSV Sanitization Import Works as Expected Importing Class"  {          
         $log = Import-LogSloth -LogFile $csvFile -SkipWarning
         $log = Import-LogSlothSanitized -LogObject $log -skipWarning
-        $log | FT -Autosize | out-host
         $log.logData.count | Should -BeExactly 128    
+        $log.logType | Should -Be "CSV"
     }
 
 }
