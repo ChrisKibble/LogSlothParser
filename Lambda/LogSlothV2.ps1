b67bf00943aab2fc444e5f7ab09b3046ea4baa84
Chris Kibble <chris@ckib.me>
2021-05-21 14:59:58 -0400

    Initial commit of Lambda function

---
+++ Lambda/LogSlothV2.ps1
@@ -0,0 +1,92 @@
+
+# PowerShell script file to be executed as a AWS Lambda function. 
+# 
+# When executing in Lambda the following variables will be predefined.
+#   $LambdaInput - A PSObject that contains the Lambda function input data.
+#   $LambdaContext - An Amazon.Lambda.Core.ILambdaContext object that contains information about the currently running Lambda environment.
+#
+# The last item in the PowerShell pipeline will be returned as the result of the Lambda function.
+#
+# To include PowerShell modules with your Lambda function, like the AWS.Tools.S3 module, add a "#Requires" statement
+# indicating the module and version. If using an AWS.Tools.* module the AWS.Tools.Common module is also required.
+
+#Requires -Modules @{ModuleName='AWS.Tools.Common';ModuleVersion='4.1.10.0'}
+#Requires -Modules LogSlothParser
+
+Try {
+    Import-Module LogSlothParser -ErrorAction Stop -Force
+    
+    # This is required because Lambda adds a Param block to the top of the code automatically
+    # and 'using' needs to be at the top.  Thanks Chris Dent! :)
+    Invoke-Expression 'using module LogSlothParser'
+} Catch {
+    Throw "Cannot Load LogSlothParser Module"
+}
+
+Function Send-Response {
+	
+    Param(
+        [int]$statusCode,
+        [array]$headers = @{"content-type"="text/plain"},
+        [string]$body
+    )
+    
+    @{
+	    "isBase64Encoded" = $false
+	    "statusCode" = $statusCode
+	    "body" = $body
+	    "headers" = $headers
+	}
+	Exit
+}
+
+# Dump Debugging Information for Testing
+Write-Host "LI"
+Write-Host (ConvertTo-Json -InputObject $LambdaInput -Compress -Depth 5)
+Write-Host "Event"
+Write-Host (ConvertTo-Json -InputObject $Event -Compress -Depth 5)
+Write-Host "LE"
+Write-Host (ConvertTo-Json -InputObject $LambdaEvent -Compress -Depth 5)
+Write-Host "LC"
+Write-Host (ConvertTo-Json -InputObject $LambdaContext -Compress -Depth 5)
+Write-Host "PSVT"
+Write-Host $PSVersionTable
+
+# Allow local ParamBody and Resource to be set for testing locally
+if($env:parambody) {
+    $LambdaInput = @{
+        "Resource" = $env:paramresource
+        "Body" = $env:parambody
+    }
+}
+
+If(-Not($LambdaInput)) {
+    Throw "No Lambda Data"
+}
+
+# Dump Debugging Information for Testing
+Write-Host "Input is $($LambdaInput)"
+Write-Host "Resource: $($LambdaInput.Resource)"
+Write-Host "Body Length: $($LambdaInput.Body.Length)"
+
+$data = $lambdaInput.Body
+$logType = Get-LogSlothType -logData $data
+
+if($logType -eq [LogType]::Nothing) {
+    Send-Response -StatusCode 400 -headers @{ "Content-Type" = "text/plain" } -body "Unable to determine log type. Sorry."
+}
+
+Switch($LambdaInput.Resource) {
+    "/log2json" {
+        $logObject = Import-LogSloth -logData $data
+        Send-Response -statusCode 200 -headers @{ "content-type" = "application/json"; "LogSloth-LogType" = $logType } -body $($logObject.LogData | ConvertTo-Json -Depth 100)
+        break
+    }
+    "/sanitize" {
+        $logObject = Import-LogSlothSanitized -logData $data
+        Send-Response -statusCode 200 -headers @{ "content-type" = "application/json"; "LogSloth-LogType" = $logType } -body $($logObject.LogData | ConvertTo-Json -Depth 100)
+    }
+    default {
+        Send-Response -statusCode 400 -headers @{ "debug-logsloth-bad-resource" = $LambdaInput.Resource } -body "I don't know what that resource is."
+    }
+}
